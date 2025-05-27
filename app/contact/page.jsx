3e9013cccb8f5e235b3f0a4fdf48ff93696b1265
mychemicalEli <elizabethblancomendez97@gmail.com>
2025-05-27 12:55:33 +0200

    fix

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -116,7 +116,7 @@ const Contact = () => {
                     </div>
                     <div className="flex-1">
                       <p className="text-white/60">{item.title}</p>
-                      <h3 className="text-sm xl:text-xl">{item.description}</h3>
+                      <h3 className="text-xl">{item.description}</h3>
                     </div>
                   </li>
                 );
